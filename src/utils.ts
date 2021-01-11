ab54edcddeabdfb5321e2ed3340a993951258861
elaygl <elaygl@gmail.com>
2021-01-11 18:50:14 +0200

    Removed unused generic

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { exec } from 'child_process';
 
 type ExecOutput = [stdout: string, stderr: string];
-export const asyncExec = async <T>(commandToExecute: string) : Promise<ExecOutput> => {
+export const asyncExec = async (commandToExecute: string) : Promise<ExecOutput> => {
     return new Promise((resolve, reject) => {
         exec(commandToExecute, (err, stdout, stderr) => {
             if (err) {return reject(err);}
