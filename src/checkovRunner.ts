470db1422af5db24eaf87ec903a0f107c3a83390
Steve Vaknin <stevevaknin21@gmail.com>
2021-01-11 16:03:22 +0200

    fix(callback to promise): change to promises, logs

--- src/checkovRunner.ts
+++ src/checkovRunner.ts
@@ -13,29 +13,32 @@ interface CheckovResponse {
     };
 }
 
-export const runCheckovScan = (fileName: string, failedHandler: (checks: FailedCheckovCheck[]) => void): void => {
-    console.log('Running checkov on', fileName);
-    const ckv = spawn('checkov', ['-f', fileName, '-o', 'json']);
-    let stdout = '';
-
-    ckv.stdout.on("data", data => {
-        stdout += data;
-    });
-		
-    ckv.stderr.on("data", data => {
-        console.warn(`stderr: ${data}`);
-    });
-		
-    ckv.on('error', (error) => {
-        console.error('Error while running Checkov', error);
-    });
-		
-    ckv.on("close", code => {
-        console.debug(`child process exited with code ${code}`);
-
-        console.debug(`task output: ${stdout}`);
-        const output: CheckovResponse = JSON.parse(stdout);
-
-        failedHandler(output.results.failed_checks);
+export const runCheckovScan = (fileName: string): Promise<CheckovResponse> => {
+    return new Promise((resolve, reject) => {
+        console.log('Running checkov on', fileName);
+        const ckv = spawn('checkov', ['-f', fileName, '-o', 'json']);
+        let stdout = '';
+	
+        ckv.stdout.on("data", data => {
+            stdout += data;
+        });
+			
+        ckv.stderr.on("data", data => {
+            console.warn(`Checkov stderr: ${data}`);
+        });
+			
+        ckv.on('error', (error) => {
+            console.error('Error while running Checkov', error);
+        });
+			
+        ckv.on("close", code => {
+            console.debug(`Checkov scan process exited with code ${code}`);
+            if (code !== 0) return reject(`Checkov exited with code ${code}`);
+	
+            console.debug(`Checkov task output: ${stdout}`);
+            const output: CheckovResponse = JSON.parse(stdout);
+	
+            resolve(output);
+        });
     });
 };
--- src/extension.ts
+++ src/extension.ts
@@ -5,24 +5,27 @@ import { applyDiagnostics } from './diagnostics';
 
 // this method is called when extension is activated
 export function activate(context: vscode.ExtensionContext): void {
-	// Set diagnostics
-	const diagnostics = vscode.languages.createDiagnosticCollection('bridgecrew-alerts');
+    // Set diagnostics
+    const diagnostics = vscode.languages.createDiagnosticCollection('bridgecrew-alerts');
     context.subscriptions.push(diagnostics);
 
-	vscode.commands.registerCommand('checkov.scan-file', () => {
-		
-		if (vscode.window.activeTextEditor) {
-			runScan(vscode.window.activeTextEditor);
-		}
-		else {
-			console.log('No active editor');
-		}
-	});
+    vscode.commands.registerCommand('checkov.scan-file', () => {
+        
+        if (vscode.window.activeTextEditor) {
+            runScan(vscode.window.activeTextEditor);
+        }
+        else {
+            console.warn('No active editor.');
+        }
+    });
 
-	function runScan(editor: vscode.TextEditor) {
-		console.log('Starting to scan');
-		const handleFailedCheck = partial(applyDiagnostics, [editor.document, diagnostics]);
-		
-		runCheckovScan(editor.document.fileName, handleFailedCheck);
-	}
+    async function runScan(editor: vscode.TextEditor) {
+        console.log('Starting to scan.');
+        try {
+            const checkovResponse = await runCheckovScan(editor.document.fileName);
+            applyDiagnostics(editor.document, diagnostics, checkovResponse.results.failed_checks);
+        } catch (error) {
+            console.error('Error occurred.', error);
+        }
+    }
 }
