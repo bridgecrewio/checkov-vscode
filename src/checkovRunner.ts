f91e36516e2b4642e3c3314f219b5569d0120cac
Steve Vaknin <stevevaknin21@gmail.com>
2021-01-11 14:40:45 +0200

    chore(eslint): indentation rule

--- src/checkovRunner.ts
+++ src/checkovRunner.ts
@@ -14,28 +14,28 @@ interface CheckovResponse {
 }
 
 export const runCheckovScan = (fileName: string, failedHandler: (checks: FailedCheckovCheck[]) => void): void => {
-		console.log('Running checkov on', fileName);
-		const ckv = spawn('checkov', ['-f', fileName, '-o', 'json']);
-		let stdout = '';
+    console.log('Running checkov on', fileName);
+    const ckv = spawn('checkov', ['-f', fileName, '-o', 'json']);
+    let stdout = '';
 
-		ckv.stdout.on("data", data => {
-			stdout += data;
-		});
+    ckv.stdout.on("data", data => {
+        stdout += data;
+    });
 		
-		ckv.stderr.on("data", data => {
-			console.warn(`stderr: ${data}`);
-		});
+    ckv.stderr.on("data", data => {
+        console.warn(`stderr: ${data}`);
+    });
 		
-		ckv.on('error', (error) => {
-			console.error('Error while running Checkov', error);
-		});
+    ckv.on('error', (error) => {
+        console.error('Error while running Checkov', error);
+    });
 		
-		ckv.on("close", code => {
-			console.debug(`child process exited with code ${code}`);
+    ckv.on("close", code => {
+        console.debug(`child process exited with code ${code}`);
 
-            console.debug(`task output: ${stdout}`);
-            const output: CheckovResponse = JSON.parse(stdout);
+        console.debug(`task output: ${stdout}`);
+        const output: CheckovResponse = JSON.parse(stdout);
 
-            failedHandler(output.results.failed_checks);
-        });
+        failedHandler(output.results.failed_checks);
+    });
 };
