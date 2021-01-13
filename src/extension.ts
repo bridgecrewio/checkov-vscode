561cb8bb1c383d5ff0c537aeb7311a5269a1628a
Steve Vaknin <stevevaknin21@gmail.com>
2021-01-13 16:40:00 +0200

    Feat(install/update ckv): install on startup

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,24 @@
 import * as vscode from 'vscode';
+import { CheckovInstalltion, installOrUpdateCheckov } from './checkovInstaller';
 import { runCheckovScan } from './checkovRunner';
 import { applyDiagnostics } from './diagnostics';
 
+const INSTALL_OR_UPDATE_COMMAND = 'checkov.install-or-update-checkov';
+
 // this method is called when extension is activated
 export function activate(context: vscode.ExtensionContext): void {
-    // Set diagnostics
+    // install or update the checkov version 
+    vscode.commands.registerCommand(INSTALL_OR_UPDATE_COMMAND, async () => {
+        try {
+            const environment: CheckovInstalltion = await installOrUpdateCheckov();
+            console.log(`finished installing checkov on ${environment.checkovPython} python environment.`);
+        } catch(error) {
+            console.error('Error occurred while trying to install Checkov', error);
+        }
+    });
+    vscode.commands.executeCommand(INSTALL_OR_UPDATE_COMMAND);
+
+    // Set diagnostics collection
     const diagnostics = vscode.languages.createDiagnosticCollection('bridgecrew-alerts');
     context.subscriptions.push(diagnostics);
 
