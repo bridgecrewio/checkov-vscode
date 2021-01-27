d3257c4b724118957a1bef7c392abee174afa9a4
Steve Vaknin <62605534+SteveVaknin@users.noreply.github.com>
2021-01-27 17:20:54 +0200

    Fix(brew): remove install with brew, add pip 3 dep (#20)

--- src/checkovInstaller.ts
+++ src/checkovInstaller.ts
@@ -2,25 +2,6 @@ import { Logger } from 'winston';
 import { join } from 'path';
 import { asyncExec } from './utils';
 
-const isMac = () => process.platform === 'darwin';
-
-const isBrewInstalled = async (): Promise<boolean> => {
-    const [stdout] = await asyncExec('brew --version');
-    return stdout.startsWith('Homebrew');
-};
-
-const updateCheckovWithBrew = async (logger: Logger): Promise<string> => {
-    try {
-        logger.info('Trying to install Checkov using Brew.');
-        await asyncExec('brew install checkov || brew upgrade checkov');
-        const [stdout] = await asyncExec('brew --prefix checkov');
-        return join(stdout.replace('\n', ''), 'bin', 'checkov');
-    } catch (error) {
-        logger.error('Failed to install or update Checkov using Brew. Error:', { error });
-        throw new Error('Failed to install or update Checkov using Brew');
-    }
-};
-
 const updateCheckovWithSystemPython = async (logger: Logger, installInPath: string): Promise<string> => {
     try {
         logger.info('Trying to install Checkov using pip3.');
@@ -32,20 +13,13 @@ const updateCheckovWithSystemPython = async (logger: Logger, installInPath: stri
     }
 };
 
-type CheckovPython = 'pip3' | 'brew';
+type CheckovPython = 'pip3';
 export interface CheckovInstallation {
     checkovPython: CheckovPython;
     path: string;
 }
 
 export const installOrUpdateCheckov = async (logger: Logger, preferredPath: string): Promise<CheckovInstallation> => {
-    if (isMac() && await isBrewInstalled()) {
-        const brewInstallPath = await updateCheckovWithBrew(logger);
-        logger.info('Checkov installed successfully using brew.');
-
-        return { checkovPython: 'brew', path: brewInstallPath };
-    }
-
     logger.info('Using pip3 to install checkov.');
     const pipInstallPath = await updateCheckovWithSystemPython(logger, preferredPath);
     logger.info('Checkov installed successfully using pip3.');
