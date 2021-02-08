005c7b835b32d420ae693b716665d1c9152c146c
elaygl <elaygl@gmail.com>
2021-02-08 17:29:44 +0200

    No need shell

--- src/utils.ts
+++ src/utils.ts
@@ -12,7 +12,7 @@ export const extensionVersion = extensionData ? extensionData.packageJSON.versio
 type ExecOutput = [stdout: string, stderr: string];
 export const asyncExec = async (commandToExecute: string, options: ExecOptions = {}) : Promise<ExecOutput> => {
     return new Promise((resolve, reject) => {
-        exec(commandToExecute, { shell: 'true', ...options }, (err, stdout, stderr) => {
+        exec(commandToExecute, options, (err, stdout, stderr) => {
             if (err) {return reject(err);}
             resolve([stdout, stderr]);
         });
