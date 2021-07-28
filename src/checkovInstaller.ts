649435cadfd93fdd2114a4ad766f7d54f8fb84c2
Mike Urbanski <mike@bridgecrew.io>
2021-07-28 14:02:29 -0500

    clean up import

--- src/checkovInstaller.ts
+++ src/checkovInstaller.ts
@@ -1,6 +1,5 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import * as vscode from 'vscode';
 import { Logger } from 'winston';
 import { getAutoUpdate, getCheckovVersion } from './configuration';
 import { asyncExec, runVersionCommand } from './utils';
